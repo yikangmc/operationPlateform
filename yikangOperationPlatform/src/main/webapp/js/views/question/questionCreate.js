911c5c2cc97efc83a4ca6e35d5cd06f53fb33cf6
liushuaic <lavedream@hotmail.com>
2016-07-12 14:29:56 +0800

    add 1.添加问题相关js文件

---
+++ yikangOperationPlatform/src/main/webapp/js/views/question/questionCreate.js
@@ -0,0 +1,21 @@
+/**
+ * @author liushuai
+ * @date 2016-07-12 14:20
+ * @desc 创建问题
+ */
+
+var Question=function(){}
+
+Question.prototype={
+		init:function(){
+			
+		},
+		checkParam:function(){
+			
+		},
+		saveQuestion:function(data){
+			
+		}
+
+
+}
