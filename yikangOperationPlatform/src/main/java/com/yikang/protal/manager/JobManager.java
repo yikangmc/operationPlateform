2aca01830513eca05980f544c1b798295dde651a
liushuaic <lavedream@hotmail.com>
2016-08-23 11:09:55 +0800

    add 1.添加新的manager

---
+++ yikangOperationPlatform/src/main/java/com/yikang/protal/manager/JobManager.java
@@ -0,0 +1,47 @@
+package com.yikang.protal.manager;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.yikang.protal.dao.JobDao;
+import com.yikang.protal.entity.Job;
+
+@Component
+public class JobManager {
+	
+	
+	@Autowired
+	private JobDao jobDao;
+
+	
+	  
+    /**
+     * @author liushuaic
+     * @date 2016-08-19 13:41
+     * @desc 获取日常任务
+     * */
+   public List<Job> getUsualJob(Long userId){
+	   return jobDao.getUsualJob(userId);
+   }
+   
+   
+   /**
+    * @author liushuaic
+    * @date 2016-08-19 13:52
+    * @desc 获取一次执行的任务
+    * */
+  public List<Job> getONCEJob(Long userId){
+	  return jobDao.getONCEJob(userId);
+   }
+
+  
+  
+  
+  
+  
+  
+  
+	
+}
