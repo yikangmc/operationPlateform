8d82319cf1aa1355e4c77dc36f3301f35d9336f5
13651237842@163.com <13651237842@163.com>
2016-09-23 17:45:25 +0800

    '更新'

--- yikangOperationPlatform/src/main/java/com/yikang/protal/message/SystemOperation.java
+++ yikangOperationPlatform/src/main/java/com/yikang/protal/message/SystemOperation.java
@@ -1,4 +1,8 @@
 package com.yikang.protal.message;
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
